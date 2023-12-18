39e891faf61a84784255a56b848aa06198dc831f
sim-sal <simone.saladino93@gmail.com>
2023-12-18 15:43:03 +0100

    add function for add new photo

--- front-end/src/pages/AppDashboard.jsx
+++ front-end/src/pages/AppDashboard.jsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import style from "../css/AppDashboard.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useAuth } from "../contexts/AuthContext"; // Importa il tuo contesto di autenticazione
+import { useAuth } from "../contexts/AuthContext";
 
 export default function AppDashboard() {
     const [photos, setPhotos] = useState([]);
     const [editPhoto, setEditPhoto] = useState(null);
+    const [isAddFormVisible, setIsAddFormVisible] = useState(false);
     const [isEditFormVisible, setIsEditFormVisible] = useState(false);
     const [categories, setCategories] = useState([]);
-    const { authToken } = useAuth(); // Estrai il token dal contesto di autenticazione
+    const { authToken } = useAuth();
 
     useEffect(() => {
         const fetchPhotos = async () => {
@@ -67,18 +68,47 @@ export default function AppDashboard() {
         setEditPhoto(null);
     };
 
-    // async function removePhoto(photoId) {
-    //     const response = await fetch(`http://localhost:3000/admin/photos/${photoId}`, {
-    //         method: "delete",
-    //     });
+    const [newPhoto, setNewPhoto] = useState({
+        title: "",
+        description: "",
+        image: "",
+        categories: []
+    });
 
-    //     if (response.ok) {
-    //         // Rimuovo la photo dalla lista locale
-    //         setPhotos(photos.filter(photo => photo.id !== photoId));
-    //     } else {
-    //         console.error("Errore durante l'eliminazione della photo");
-    //     }
-    // }
+    const openAddForm = () => {
+        setIsAddFormVisible(!isAddFormVisible);
+    };
+
+    const addNewPhoto = async () => {
+        try {
+            // Verifica se il titolo è presente nel nuovoPhoto
+            if (!newPhoto.title) {
+                console.error("Title is required");
+                return;
+            }
+
+            const response = await axios.post(
+                "http://localhost:3000/admin/photos",
+                newPhoto,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                }
+            );
+
+            console.log("Photo added successfully:", response.data);
+
+            // Aggiorna la lista delle foto (se necessario)
+            setPhotos((prevPhotos) => [...prevPhotos, response.data]);
+
+            // Chiudi il form di aggiunta
+            setIsAddFormVisible(false);
+        } catch (error) {
+            console.error("Error adding photo:", error);
+            console.log("Response data:", error.response.data);
+        }
+    };
 
     const handleDeleteClick = async (photoId) => {
         try {
@@ -96,11 +126,92 @@ export default function AppDashboard() {
 
     return (
         <div className={`container-fluid ${style.dashboard}`}>
-            <div>
-                <button className={style.add_button}>
-                    <span><strong>Add Photo</strong></span>
-                </button>
-            </div>
+            {isAddFormVisible && (
+                <div className={`${style.add_edit_FormContainer} ${style.show}`}>
+                    <div>
+                        <h2>Add Photo</h2>
+                        <form
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                addNewPhoto();
+                            }}
+                        >
+                            <div>
+                                <label><strong>Image URL:</strong></label>
+                                <input
+                                    type="text"
+                                    name="imageUrl"
+                                    value={newPhoto.image}
+                                    onChange={(e) => setNewPhoto({ ...newPhoto, image: e.target.value })}
+                                    required
+                                />
+                            </div>
+                            <div >
+                                <label><strong>Title:</strong></label>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    value={newPhoto.title}
+                                    onChange={(e) => setNewPhoto({ ...newPhoto, title: e.target.value })}
+                                    required
+                                />
+                            </div>
+                            <div >
+                                <label><strong>Description:</strong></label>
+                                <textarea
+                                    name="description"
+                                    value={newPhoto.description}
+                                    onChange={(e) => setNewPhoto({ ...newPhoto, description: e.target.value })}
+                                    required
+                                ></textarea>
+                            </div>
+                            <div>
+                                <label><strong>Categories:</strong></label>
+                                <div className="row">
+                                    {categories.map((category) => (
+                                        <div className="col-4" key={category.id}>
+                                            <input
+                                                type="checkbox"
+                                                id={`category_${category.id}`}
+                                                name="categories"
+                                                value={category.id}
+                                                checked={newPhoto.categories.includes(category.id)}
+                                                onChange={(e) => {
+                                                    const isChecked = e.target.checked;
+                                                    setNewPhoto((prevPhoto) => {
+                                                        if (isChecked) {
+                                                            return { ...prevPhoto, categories: [...prevPhoto.categories, category.id] };
+                                                        } else {
+                                                            return { ...prevPhoto, categories: prevPhoto.categories.filter((catId) => catId !== category.id) };
+                                                        }
+                                                    });
+                                                }}
+                                            />
+                                            <label htmlFor={`category_${category.id}`}>
+                                                {category.name}
+                                            </label>
+                                        </div>
+                                    ))}
+                                </div>
+
+                            </div>
+
+                            <div className="d-flex justify-content-between mt-4">
+                                <button className={style.save_button} type="submit">Save</button>
+
+                                <button className={style.dismit_button} onClick={() => setIsAddFormVisible(false)}>
+                                    X
+                                </button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            )}
+
+            <button className={style.add_button} onClick={openAddForm}>
+                <span><strong>Add Photo</strong></span>
+            </button>
 
             <div className="container">
                 <div className="row">
@@ -108,7 +219,7 @@ export default function AppDashboard() {
                         <div key={photo.id} className={`card col-5 mx-5 my-5 ${style.mod_card}`}>
                             <img
                                 className={style.card_img}
-                                src={`/${photo.image}`}
+                                src={(photo.image && photo.image.startsWith('http')) ? photo.image : `/${photo.image}`}
                                 alt={photo.title}
                             />
                             <div className={style.card_main}>
@@ -116,11 +227,15 @@ export default function AppDashboard() {
                                 <p>{photo.description}</p>
                                 <span>
                                     Categories:{" "}
-                                    {photo.categories.map((category) => (
-                                        <ul key={category.id}>
-                                            <li>{category.name}</li>
-                                        </ul>
-                                    ))}
+                                    {photo.categories ? (
+                                        photo.categories.map((category) => (
+                                            <ul key={category.id}>
+                                                <li>{category.name}</li>
+                                            </ul>
+                                        ))
+                                    ) : (
+                                        <span>No categories available</span>
+                                    )}
                                 </span>
                                 <div className={style.card_buttons}>
                                     <button className={style.edit_button} onClick={() => handleEditClick(photo)}> <span><strong>Edit</strong></span> </button>
@@ -143,7 +258,7 @@ export default function AppDashboard() {
 
             {
                 isEditFormVisible && (
-                    <div className={`${style.editFormContainer} ${style.show}`}>
+                    <div className={`${style.add_edit_FormContainer} ${style.show}`}>
                         <div>
                             <h2>Edit Photo</h2>
                             <form
